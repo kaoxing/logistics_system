079031f7213266eff8963e275911866d7d6497c0
kaoxing <3032738029@qq.com>
2022-11-26 20:58:00 +0800

    添加注释

--- app01/my_tools.py
+++ app01/my_tools.py
@@ -9,18 +9,32 @@ import coder
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+#   例子，该方法在数据库中寻找用户
+# def user_exist(id, pwd):
+#    cursor = connection.cursor()
+#    sql = "select * from shopper_table where shopper_num='{}'".format(id)  #sql语句
+#    cursor.execute(sql)    #执行sql
+#    rows = cursor.fetchall()   #获取执行结果rows,可以打印出来看看
+#    if len(rows) != 0 and rows[0][2] == pwd:
+#        return rows[0][1]
+#    return None
+
+
 def user_exist(id, pwd):
     # todo 用户登录，返回为None则不存在，否则返回用户昵称
+    cursor = connection.cursor()
     return "xing"
 
 
 def poster_exist(id, pwd):
-    # todo 跑腿登录，返回为None则不存在，否则返回用户昵称
+    # todo 跑腿登录，返回为None则不存在，
+    cursor = connection.cursor()
     return "Xing"
 
 
 def manager_exist(id, pwd):
     # todo 跑腿登录，返回为None则不存在，否则返回用户昵称
+    cursor = connection.cursor()
     return "kao"
 
 
